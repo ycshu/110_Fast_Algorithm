b2194d949f4ba9cf2937147069c8e507cc75bf96
ycshu <ycshu@mail.ncku.edu.tw>
2022-03-01 21:42:44 +0800

    Add the comment in 1_hello.c

--- 1_hello.c
+++ 1_hello.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>	// include standard input and output library for printf
 
 int main()
 {
-	printf("Hello world!\n");
+	printf("Hello world!\n");	// print Hello World to screen
 	return 0;
 }
