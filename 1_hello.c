8e01a47c1b76c45a422f3962e0cbcb0cc085433a
ycshu <ycshu@mail.ncku.edu.tw>
2022-03-01 16:21:59 +0800

    Update 1_hello.c
    comments and print a integer number

--- 1_hello.c
+++ 1_hello.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // include stdio.h library for printf
 
 int main()
 {
-	printf("Hello world!\n");
+	printf("Hello world!\n"); // print hello world to screen!
+	printf("%d\n", 3/2);
 	return 0;
 }
