feb5bc497b4e80432ed3d45da52b73d18992fc0b
chenyixuan0119 <100676477+chenyixuan0119@users.noreply.github.com>
2022-03-05 03:41:38 +0800

    l16091019
    利用尤拉公式把e^(-i*2*pi*k*n/N)轉換成cos跟sin直接計算

--- 5_practice.c
+++ 5_practice.c
@@ -2,38 +2,48 @@
 #include <stdlib.h>	// for memory allocation
 #include <time.h>	// for time calculation
 #include <math.h>	// for sine and cosine functions
+
 int main() {
 	// Declare all the variables
 	int k, n, N;
 	double *x, *yr, *yi;
-	time_t t;
+	time_t t, t1;
+	int j;
 
 	// Input the number N
-	
+	printf("Input the number N :");
+	scanf("%d",&N);
 	
 	// Locate the memory for x, yr, yi;
-
+	x = (double *) malloc(N*sizeof(double));
+	yr = (double *) malloc(N*sizeof(double));
+	yi = (double *) malloc(N*sizeof(double));
+	
 	// Initial setting for x, for example, x[k] = k
-	
-	
-	
+	for(k=0;k<N;k++){
+		x[k] = k;
+	}
 	
 	t = clock();	
 	// yr[n]+i*yi[n] = sum(exp(-i 2 Pi k n / N)*x[k], k=0..N-1), n=0..N-1 
-
-
-
-
-
-
-
+	for(n=0;n<N;n++){
+		yr[n] = 0.0;
+		yi[n] = 0.0;
+		for(k=0;k<N;k++){
+			yr[n] += cos(2.0*M_PI*k*n/N)*x[k];
+			yi[n] -= sin(2.0*M_PI*k*n/N)*x[k];
+		} 
+		
+	}
 	// output the results
 	t = clock() - t;
 	printf("%d ms for discrete Fourier Transform of %d elements\n", t, N);
-
+	
+	
 	// free the memory located by x, yr, yi
-	
-	
+	free(x);
+	free(yr);
+	free(yi);
 	
 	return 100;
 }
