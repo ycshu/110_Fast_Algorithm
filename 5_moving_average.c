fc04c82f8156ac643adf2e8e3ca7ebc03a4a1759
yuchen-shu <100857696+yuchen-shu@users.noreply.github.com>
2022-03-05 10:53:26 +0800

    Update 5_moving_average.c (#4)
    * Update 5_moving_average.c
    
    * version 1 of moving averaging
    
    * 不要再上傳資料了！！
    
    CSV檔案跟EXCEL檔案都不要再上傳了！
    
    Co-authored-by: ycshu <ycshu@mail.ncku.edu.tw>

--- 5_moving_average.c
+++ 5_moving_average.c
@@ -8,24 +8,33 @@ int main() {
 	double *x, *y;
 	time_t t;
 
-	// Input the number N
-	
-	
-	// Locate the memory for x, y;
+	// Input the number N, M
+	printf("N M= ");	
+	scanf("%d %d", &N, &M);
 
-	
+	// Locate the memory for x, y;
+	x = (double *) malloc(N*sizeof(double));
+	y = (double *) malloc(N*sizeof(double));
 	// Initial setting for x[k] = sin(2*Pi*k/N), k = 0..N-1
-	
-	
+	for(k=0;k<N;++k) {
+		x[k] = sin(2*M_PI*k/N);
+	}
 	
 	t = clock();	
 	// y[n] = sum(x[k], k=n-M..n+M)/(2M+1), n=0..N-1 
 	// zero padding for x[k] if k<0 or k>=N
-
+	for(n=0;n<N;n++){
+		y[n] = 0.0;
+		for(k=n-M;k<=n+M;++k){
+			if(k>=0 || k<N)
+				y[n] = y[n] + x[k];
+		}
+		y[n] = y[n]/(2*M+1);
+	}
 
 	// Output the results
 	t = clock() - t;
-	printf("\n");
+	printf("y[0]=%f, y[1]=%f\n",y[0],y[1]);
 
 	// free the memory located by x, y
 	
