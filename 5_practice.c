0879cda8a153815709fe183c10723393597d699d
jctu <41058334+Dofolk@users.noreply.github.com>
2022-03-09 23:08:47 +0800

    Done 5_practice.c with version 1
    做出基本款的加總
    只留意加減法的加速，沒有從演算法上面去加速。

--- 5_practice.c
+++ 5_practice.c
@@ -18,27 +18,34 @@ int main() {
 	yi = (double *) malloc(N*sizeof(double));
 	
 	// Initial setting for x, for example, x[k] = k
-	
-	
-	
+	for(int i = 0; i<N; i++){
+		x[i] = i;
+		yr[i] = 0.0;
+		yi[i] = 0.0;
+	}
 	
 	t = clock();	
 	// yr[n]+i*yi[n] = sum(exp(-i 2 Pi k n / N)*x[k], k=0..N-1), n=0..N-1 
-
-
-
-
-
-
+	//					cos(2 PI k n / N)		i sin(2 PI k n / N)
+	double tmp = 2*M_PI/N;
+	double theta;
+	for (int i = 1; i<N; i++){
+ 		for(int j = 1; j<N; j++){
+			theta = tmp*i*j;
+			yr[i] += cos(theta)*x[i];
+			yi[i] -= sin(theta)*x[i];
+		}
+	}
 
 	// output the results
 	t = clock() - t;
 	printf("%d ms for discrete Fourier Transform of %d elements\n", t, N);
 
 	// free the memory located by x, yr, yi
-	
-	
-	
-	return 100;
+	free(yi);
+	free(yr);
+	free(x);
+
+	return 0;
 }
 
