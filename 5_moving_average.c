4e96e8724d9368e6bd8c868774a8224d15f70577
Eugene Lin <lin.eugene.l.e@gmail.com>
2022-03-10 22:31:37 +0800

    Finish 5_moving_average.c
    In this assignment, I wrote two versions of moving average one
    in O(NM) and the other is in O(N). I use result of O(NM) to test
    the result of O(N) because I think the method to write O(NM) is
    easier and unlikely to have bug in there.

--- 5_moving_average.c
+++ 5_moving_average.c
@@ -2,34 +2,95 @@
 #include <stdlib.h>	// for memory allocation
 #include <time.h>	// for time calculation
 #include <math.h>	// for sine and cosine functions
-int main() {
+#include <string.h>
+
+#define TEST
+
+#ifdef TEST
+#include <assert.h>
+#endif
+
+int main(int argc, const char *argv[]) {
 	// Declare all the variables
 	int k, n, M, N;
 	double *x, *y;
 	time_t t;
 
-	// Input the number N
+	// Input the number N, M
+    printf("N = ");
+    scanf("%d", &N);	
+    printf("M = ");
+    scanf("%d", &M);
 	
-	
+    double size = 2*M + 1;
 	// Locate the memory for x, y;
+    x = (double*)malloc(sizeof(double)*N);
+    y = (double*)malloc(sizeof(double)*N);
 
+
+#ifdef TEST
+    double *y_test = (double*)malloc(sizeof(double)*N);
+    memset(y, 0, sizeof(double)*N);
+#endif
 	
 	// Initial setting for x[k] = sin(2*Pi*k/N), k = 0..N-1
-	
+    for(int i = 0; i < N; ++i){
+        x[i] = sin(2*M_PI*i/(double)N);
+    }
+    memset(y, 0, sizeof(double)*N);
+
+#ifdef TEST
+    // use O(NM) moving average to perform test
+    for(int n = 0; n < N; ++n){
+        for(int k = n-M, upper_bound = n+M; k <= upper_bound; ++k){
+            if(k >= 0 && k < N){
+                y_test[n] += x[k];
+            }
+        }
+        y_test[n] /= size;
+    }
+#endif
 	
 	
 	t = clock();	
 	// y[n] = sum(x[k], k=n-M..n+M)/(2M+1), n=0..N-1 
 	// zero padding for x[k] if k<0 or k>=N
+    //initialize y[0]
+    for(int i = 0; i <= M; ++i) y[0] += x[i];
+
+    for(int i = 1; i < N; ++i){
+        // middle is i
+        if(i - 1 - M >= 0){
+            y[i] = y[i - 1] - x[i - 1 - M];
+        }else{
+            y[i] = y[i - 1];
+        }
+        
+        if(i + M < N){
+            y[i] += x[i + M];
+        }
+    }
+    
+    for(int i = 0; i < N; ++i) y[i] /= size;
 
 
 	// Output the results
 	t = clock() - t;
+    for(int i = 0; i < N; ++i){
+#ifdef TEST
+        assert(y[i] - y_test[i] < 0.00001); // 0.00001 for floating number bias
+#endif
+        printf("%.3f\n", y[i]);
+    }
 	printf("\n");
 
 	// free the memory located by x, y
-	
-	
+    free(x);
+    free(y);
+
+#ifdef TEST
+    free(y_test);
+#endif
 	
 	return 100;
 }
