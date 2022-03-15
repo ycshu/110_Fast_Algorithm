5153c81b572b1bbac30648967660329b3a8d4b10
chenyixuan0119 <100676477+chenyixuan0119@users.noreply.github.com>
2022-03-16 04:44:53 +0800

    update2_6_sorting.c
    M1是利用quick_sort_median求中位數，而M2是利用quick_sort_median1求中位數，差別在求n為偶數時，M1是在 function 裡從大於第n/2大的數中找第n/2+1大的數字，而M2則是直接在main裡面全部從x[0]到x[N-1]在重新跑過一遍找第n/2+1大的數字，但後來發現兩個會一樣快，是因為M2在前n/2項不會做更動，所以其實就跟M1在找第n/2+1大的數做的事情是一樣的，四分位數就直接利用 M1 去搜尋

--- 6_sorting.c
+++ 6_sorting.c
@@ -3,12 +3,13 @@
 #include <time.h>	// for time calculation
 #include <math.h>	// for sine and cosine functions
 int quick_sort(double *x, int L, int R);
-double quick_sort_median(double *x1, int L, int R, int M1,int bl);
+double quick_sort_median(double *x, int L, int R, int M1,int bl);
+double quick_sort_median1(double *x, int L, int R, int M1);
 int main() {
 	// Declare all the variables
 	int k, m, n, N;
-	double *x, *y,*x1, z, p , med, med1;
-	time_t t;
+	double *x, *y, z, p , med, med1, med2,q1,q3;
+	time_t t,t1,t2;
 
 	// Input the number N
 	printf("Input N: ");
@@ -17,14 +18,12 @@ int main() {
 	// Locate the memory for x and y;
 	x = (double *) malloc(N*sizeof(double));
 	y = (double *) malloc(N*sizeof(double));
-	x1 = (double *) malloc(N*sizeof(double));
 	// Initial setting for x, for example, x[k] = 1.0*rand()/RAND_MAX
+
 	srand( time(NULL) );
 	for(k=0;k<N;k++){
-		x[k] = y[k] = x1[k] = 1.0*rand()/RAND_MAX;
-	}
-	
-	t = clock();	
+		x[k] = y[k] = 1.0*rand()/RAND_MAX;
+	}	
 	// sorting x;  
 	for(n=0;n<N;n++) {
 		for(k=n+1;k<N;k++) {
@@ -35,54 +34,73 @@ int main() {
 			}
 		}
 	}
-	t = clock() - t;
-	// print y, x, and time
-	printf("Sorting %d elements: %e s\n", N, 1.0*t/CLOCKS_PER_SEC);
-	if(N<=10) {
-		printf("y \t\t x\n");
-		for(k=0;k<N;++k) {
-			printf("%f\t%f\n",y[k],x[k]);
-		}
-	}
-
 	for(k=0;k<N;++k){
 		x[k] = y[k];
 	}
 	
-	// quick sort x 
-	t = clock();
 	quick_sort(x,0,N);
-	t = clock() - t;
-	// x[0] is at the correct location!
-	printf("Quick Sorting %d elements: %e s\n", N, 1.0*t/CLOCKS_PER_SEC);
-	if(N<=10) {
+	
+	if(N<=20) {
 		printf("y \t\t x\n");
 		for(k=0;k<N;++k) {
-			printf("%f\t%f\n",y[k],x[k]);
+			printf("%d %f\t%f\n",k,y[k],x[k]);
 		}
 	}
 	
-	t = clock();
-	if( N % 2 != 0){
-		med1 = quick_sort_median(x1,0,N,(N-1)/2,1);
-	}
-	else{
-		med1 = quick_sort_median(x1,0,N,(N-1)/2,0);
-	}
-	t = clock() - t;
 	if( N % 2 != 0){
 		med = x[N/2];
 	}
 	else{
 		med = (x[(N/2)-1]+x[N/2])/2.0;
 	}
-	printf("quick_sort_median spends : %e s\n", N, 1.0*t/CLOCKS_PER_SEC);
-	printf("The error is %f \n",med-med1);
 
+	for(k=0;k<N;++k){
+		x[k] = y[k];
+	}
+	t = clock();
+	if( N % 2 != 0){
+		med1 = quick_sort_median(x,0,N,(N-1)/2,1);
+	}
+	else{
+		med1 = quick_sort_median(x,0,N,(N-1)/2,0);
+	}
+	t = clock() - t;
+	
+	t2 = clock();
+	if( (((N-1)/2)%2 != 0 && N%2 != 0) || (N % 2 ==0 && (N/2)%2 != 0)){
+		q1 = quick_sort_median(x,0,N/2,((N/2)-1)/2,1); 
+		q3 = quick_sort_median(x,N/2+1,N,3*N/4,1); 
+	}
+	else{
+		q1 = quick_sort_median(x,0,N/2,((N/2)-1)/2,0);
+		q3 = quick_sort_median(x,(N-1)/2+1,N,(3*N-1)/4,0);
+	}
+	t2 = t2-clock();
+	
+	for(k=0;k<N;++k){
+		x[k] = y[k];
+	}
+	
+	t1 = clock();
+	if( N % 2 != 0){
+	med2 = quick_sort_median1(x,0,N,(N-1)/2);
+	  }
+	else{
+	  	med2 = quick_sort_median1(x,0,N,(N-1)/2);
+	   	med2 = med2 + quick_sort_median1(x,0,N,N/2);
+	   	med2 = med2/2.0;
+	}
+	t1 = t1-clock();
+
+	printf("quick_sort_median spends : %e s\n", N, 1.0*t/CLOCKS_PER_SEC);
+	printf("quick_sort_median1 spends : %e s\n", N, 1.0*t1/CLOCKS_PER_SEC);
+	printf("The error with quick_sort_median is %f \n",med-med1);
+	printf("The error with quick_sort_median1 is %f \n",med-med2);
+	printf("q1 = %f q3 = %f time : %e \n", q1,q3,1.0*t2/CLOCKS_PER_SEC);
 	// free the memory located by x, y
 	free(x);
 	free(y);	
-	free(x1);
+
 	return 100;
 }
 
@@ -113,41 +131,75 @@ int quick_sort(double *x, int L, int R){
 	return 0;
 }
 
-double quick_sort_median(double *x1, int L, int R, int M1,int bl){
+double quick_sort_median(double *x, int L, int R, int M1,int bl){
 	int k, n,m;
 	double z, p;
 	if(L >= R){
 		return 0;
 	}
 
-	p = x1[R-1];
+	p = x[R-1];
 	n = L;
 	for(k=L;k<R-1;k++) {
 		// put all elements smaller than p from the beginning
-		if (x1[k] < p) {
-			z = x1[n];
-			x1[n] = x1[k];
-			x1[k] = z;
+		if (x[k] < p) {
+			z = x[n];
+			x[n] = x[k];
+			x[k] = z;
 			n++;
 		}
 	}
-	z = x1[n];
-	x1[n] = x1[R-1];
-	x1[R-1] = z;
+	z = x[n];
+	x[n] = x[R-1];
+	x[R-1] = z;
 
 	if(n > M1){
-		quick_sort_median(x1,L,n,M1,bl);
+		quick_sort_median(x,L,n,M1,bl);
 	}
 	else if(n < M1){
-		quick_sort_median(x1,n+1,R,M1,bl);
+		quick_sort_median(x,n+1,R,M1,bl);
 	}
 	else{
 		if(bl){
-			return x1[n];
+			return x[n];
 		}
 		else{
-			quick_sort_median(x1,n+1,R,M1+1,bl);
-			return (x1[n]+x1[n+1])/2.0;
+			quick_sort_median(x,n+1,R,M1+1,bl);
+			return (x[n]+x[n+1])/2.0;
 		}
 	}
+}
+
+double quick_sort_median1(double *x, int L, int R, int M1){
+	int k, n,m;
+	double z, p;
+	if(L >= R){
+		return 0;
+	}
+
+	p = x[R-1];
+	n = L;
+	for(k=L;k<R-1;k++) {
+		// put all elements smaller than p from the beginning
+		if (x[k] < p) {
+			z = x[n];
+			x[n] = x[k];
+			x[k] = z;
+			n++;
+		}
+	}
+	z = x[n];
+	x[n] = x[R-1];
+	x[R-1] = z;
+
+	if(n > M1){
+		quick_sort_median1(x,L,n,M1);
+	}
+	else if(n < M1){
+		quick_sort_median1(x,n+1,R,M1);
+	}
+	else{
+		return x[n];
+	}
+	
 }
