d6ccfa1cc156dc4334567f3b1e7e141d9b14a721
danny0926 <42000puzzles@gmail.com>
2022-03-17 20:52:20 +0800

    Update 5_practice.c
    加速

--- 5_practice.c
+++ 5_practice.c
@@ -5,7 +5,7 @@
 int main() {
 	// Declare all the variables
 	int k, n, N;
-	double *x, *yr, *yi;
+	double *x, *yr, *yi, a, b, an, bn, temp, P, c, s;
 	time_t t;
 
 	// Input the number N
@@ -21,14 +21,26 @@ int main() {
 	
 	t = clock();	
 	// yr[n]+i*yi[n] = sum(exp(-i 2 Pi k n / N)*x[k], k=0..N-1), n=0..N-1 
+	a = cos(2*M_PI/N); b = -sin(2*M_PI/N);
 	for(n = 0; n < N; n++){
 		yr[n] = 0.0;
 		yi[n] = 0.0;
+		P = 2*M_PI/N;
+		an = 1; bn = 0;
+		c = 1; s = 0;
 		for(k = 0; k < N; k++){
-			double theta = (-2*M_PI*k*n)/N;
-			yr[n] = yr[n] + cos(theta)*x[k];
-			yi[n] = yi[n] + sin(theta)*x[k];
+			yr[n] += c*x[k];
+			yi[n] -= s*x[k];
+			temp = c * an - s * bn;
+			s = c * bn + s * an;
+			c = temp;
+//			double theta = (-2*M_PI*k*n)/N;
+//			yr[n] = yr[n] + cos(theta)*x[k];
+//			yi[n] = yi[n] + sin(theta)*x[k];
 		}
+		temp =  an * a - bn * b;
+		bn = an * b + bn * a;
+		an = temp; 
 	}
 
 	// output the results
