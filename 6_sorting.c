748126d9033e6f702e16855736cd471344c624cf
danny0926 <42000puzzles@gmail.com>
2022-03-19 19:35:40 +0800

    Update 6_sorting.c

--- 6_sorting.c
+++ 6_sorting.c
@@ -5,9 +5,11 @@
 
 
 
-int quick_sort(double*, int, int);
-void SWAP(double, double);
-
+void quick_sort(double*, int, int);			//quick sort
+int Partition(double*, int, int);			//sub algo of quick sort
+double Select(double*, int, int, int);		// find the i-th number
+double Find_Med(double*, int, int);			//find the median
+double QD(double*, int, int);				// find the Quartile Deviation
 int main() {
 	// Declare all the variables
 	int k, m, n, N;
@@ -27,7 +29,11 @@ int main() {
 	for(k=0;k<N;++k){
 		x[k] = y[k] = 1.0*rand()/RAND_MAX;
 	}
-	
+	printf("origin : \n");
+	for(k = 0; k < N; k++){
+		printf("%f\n", x[k]);
+	}
+	printf("----------------------\n");
 	t = clock();	
 	// sorting x;  
 	for(n=0;n<N;++n) {
@@ -40,49 +46,51 @@ int main() {
 		}
 	}
 	t = clock() - t;
-
+	
 	// print y, x, and time
-	printf("Sorting %d elements: %f s\n", N, 1.0*t/CLOCKS_PER_SEC);
-	if(N<10) {
-		printf("y \t\t x\n");
-		for(k=0;k<N;++k) {
-			printf("%f\t%f\n",y[k],x[k]);
-		}
+	printf("Bubble sort %d elements: %f s\n", N, 1.0*t/CLOCKS_PER_SEC);
+	
+	printf("bubble sort : \n");
+	for(k=0;k<N;++k) {
+		printf("%f\n",x[k]);
 	}
+	
 
 	for(k=0;k<N;++k){
 		x[k] = y[k];
 	}
-	t = clock();
-		
-	// put x[N-1] at the right location
-	p = x[N-1]; n = 0;
-	for(k=0;k<N-1;k++) {
-		// put all elements smaller than p from the beginning
-		if (x[k] < p) {
-			printf("swap %d <-> %d\n",k,n);
-			z = x[n];
-			x[n] = x[k];
-			x[k] = z;
-			n++;
-		}
-	}
-	z = x[n];
-	x[n] = x[N-1];
-	x[N-1] = z;
+	printf("------------------------\n");
 	
-	// x[0] is at the correct location!
-	if(N<10) {
-		printf("y \t\t x\n");
-		for(k=0;k<N;++k) {
-			printf("%f\t%f\n",y[k],x[k]);
-		}
+	t = clock();
+	
+	quick_sort(x, 0, N - 1);
+	printf("quick sort : \n");
+	for(k = 0; k < N; k++){
+		printf("%f\n",x[k]);
 	}
 	// sorting
-
-
-	t = clock() - t;	
+	t = clock() - t;
 	
+	
+	printf("Quick sort %d elements: %f s\n", N, 1.0*t/CLOCKS_PER_SEC); 
+	
+	for(k=0;k<N;++k){
+		x[k] = y[k];
+	}
+	
+	t = clock();
+	double Med = Find_Med(x, 0, N - 1);
+	printf("-----------------------\n");
+	printf("Median = %f\n", Med); 
+	t = clock() - t;	
+	printf("Find median in %d elements: %f s\n", N, 1.0*t/CLOCKS_PER_SEC);
+	
+	printf("-----------------------\n");
+	t = clock();
+	printf("Quartile Deviation = %f\n", QD(x, 0, N - 1));
+	t = clock() - t;
+	printf("Find Quartile Deviation in %d elements: %f s\n", N, 1.0*t/CLOCKS_PER_SEC);
+		
 
 
 	// free the memory located by x, y
@@ -92,39 +100,76 @@ int main() {
 	return 100;
 }
 
-int quick_sort(double *x, int L, int R){
+void quick_sort(double *x, int P, int r){
 	double temp;
-	if(L < R){
-		double pk = x[L];
-		int i = L, j = R + 1;
-		while(i < j){
-			while(x[i] < pk) i++;
-			while(x[j] > pk) j--;
-			if(i < j){
-				temp = x[i];
-				x[i] = x[j];
-				x[j] = temp;
-			}
-		}
-		temp = x[L];
-		x[L] = x[j];
-		x[j] = temp; 
-//		SWAP(x[L], x[j]);
-		quick_sort(x, L, j - 1);
-		quick_sort(x, j + 1, R);
+	if(P < r){
+		int q = Partition(x, P, r);
+		quick_sort(x, P, q - 1);
+		quick_sort(x, q + 1, r);
 	}
 }
 
-//void SWAP(double X, double Y){
-//	X ^= Y;
-//	Y ^= X;
-//	X ^= Y;
-//}
+int Partition(double *x, int P, int r){
+	double a = x[r];
+	int i = P - 1, k;
+	for(k = P; k < r; k++){
+		if(x[k] <= a){
+			i++;
+			double temp;
+			temp = x[i];
+			x[i] = x[k];
+			x[k] = temp;
+		}
+	}
+	double temp;
+	temp = x[i + 1];
+	x[i + 1] = x[r];
+	x[r] = temp;
+	return (i + 1);
+}
 
+double Select(double *x, int P, int r, int i){
+	if(P < r){
+		int q = Partition(x, P, r);
+		int k = q - P + 1;
+		if(i == k) return x[q];
+		else if(i < k) return Select(x, P, q - 1, i);
+		else return Select(x, q + 1, r, i - k);
+	}
+	else{
+		return x[r];
+	}
+}
 
+double Find_Med(double *x, int P, int r){
+	int number = r - P + 1;
+	if(number % 2 == 0){
+		double right = Select(x, P, r, number/2 + 1);
+		double left = Select(x, P, r, number/2);
+		return (right + left)/2;
+	}
+	else{
+		return Select(x, P, r, number/2 + 1);
+	}
+}
 
-
-
+double QD(double *x, int P, int r){		// ¥|¤À¦ì®t  Quartile Deviation, QD 
+	int number = r - P + 1;
+	if(number/2 == 0){
+		double Q1 = Find_Med(x, P, number/2 - 1);
+		printf("Q1 = %f\n", Q1);
+		double Q3 = Find_Med(x, number/2, r);
+		printf("Q3 = %f\n", Q3);
+		return Q3 - Q1;	
+	}
+	else{
+		double Q1 = Find_Med(x, P, number/2 - 1);
+		printf("Q1 = %f\n", Q1);
+		double Q3 = Find_Med(x, (number + 1)/2, r);
+		printf("Q3 = %f\n", Q3);
+		return Q3 - Q1;	
+	}		
+}
 
 
 
